7ab7041d7049c44e79d8d3618b4f13e3f0ae9db7
Nick <nicolas.cinera@gmail.com>
2022-08-20 08:55:43 -0500

    Initial Commit

---
+++ src/main/java/com/body/improvement/club/BodyImprovementApplication.java
@@ -0,0 +1,13 @@
+package com.body.improvement.club;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BodyImprovementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BodyImprovementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/body/improvement/club/BodyImprovementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.body.improvement.club;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BodyImprovementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
