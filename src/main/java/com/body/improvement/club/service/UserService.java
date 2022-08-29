877b86a2c05ac9ecf5e135b37a7d4ada624d3c66
Nick <nicolas.cinera@gmail.com>
2022-08-28 22:23:39 -0400

    Small Update

--- src/main/java/com/body/improvement/club/service/UserService.java
+++ src/main/java/com/body/improvement/club/service/UserService.java
@@ -1,9 +1,23 @@
 package com.body.improvement.club.service;
 
+import com.body.improvement.club.entity.User;
+import com.body.improvement.club.repository.UserRepository;
 import lombok.extern.log4j.Log4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.Collection;
 
 @Service
 @Log4j
 public class UserService implements ServiceDelegator{
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public ResponseEntity<Collection<User>> getAllUsers(){
+        Collection<User> payload = userRepository.findAll();
+        return ResponseEntity.ok(payload);
+    }
 }
