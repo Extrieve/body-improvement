7dbb8f190d413bbef0d757d08fe3fcee61a5c8c5
Nick <nicolas.cinera@gmail.com>
2022-09-18 21:25:38 -0400

    Logger and Empty Payloads

--- src/main/java/com/body/improvement/club/service/UserService.java
+++ src/main/java/com/body/improvement/club/service/UserService.java
@@ -2,6 +2,8 @@ package com.body.improvement.club.service;
 
 import com.body.improvement.club.entity.User;
 import com.body.improvement.club.repository.UserRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -14,28 +16,66 @@ public class UserService implements ServiceDelegator{
     @Autowired
     private UserRepository userRepository;
 
+    private final Logger logger = LogManager.getLogger(UserService.class);
+
     public ResponseEntity<User> getUserByUsername(String username){
-        User payload = userRepository.findUserByUsername(username);
-        return ResponseEntity.ok().body(payload);
+        logger.info("Fetching user by username: " + username);
+
+        try{
+            User user = userRepository.findUserByUsername(username);
+            return ResponseEntity.ok(user);
+        } catch (Exception e) {
+            logger.error("Error fetching user by username: " + username);
+            return ResponseEntity.badRequest().build();
+        }
     }
 
     public ResponseEntity<Collection<User>> getUsersByFirstNameAndLastName(String firstName, String lastName){
-        Collection<User> payload = userRepository.findUserByFirstNameAndLastName(firstName, lastName);
-        return ResponseEntity.ok().body(payload);
+        logger.info("Fetching users by first name: " + firstName + " and last name: " + lastName);
+
+        try{
+            Collection<User> users = userRepository.findUserByFirstNameAndLastName(firstName, lastName);
+            return ResponseEntity.ok(users);
+        } catch (Exception e) {
+            logger.error("Error fetching users by first name: " + firstName + " and last name: " + lastName);
+            return ResponseEntity.badRequest().build();
+        }
     }
 
     public ResponseEntity<Collection<User>> getUsersByFirstNameContaining(String firstName){
-        Collection<User> payload = userRepository.findUserByFirstNameContaining(firstName);
-        return ResponseEntity.ok().body(payload);
+        logger.info("Fetching users by first name: " + firstName);
+
+        try{
+            Collection<User> users = userRepository.findUserByFirstNameContaining(firstName);
+            return ResponseEntity.ok(users);
+        } catch (Exception e) {
+            logger.error("Error fetching users by first name: " + firstName);
+            return ResponseEntity.badRequest().build();
+        }
+
     }
 
     public ResponseEntity<Collection<User>> getAllUsers(){
+        logger.info("Fetching all users");
         Collection<User> payload = userRepository.findAll();
+
+        if(payload.isEmpty()){
+            logger.error("No users found");
+            return ResponseEntity.noContent().build();
+        }
+
         return ResponseEntity.ok(payload);
     }
 
     public ResponseEntity<Collection<User>> getUserByUsernameContaining(String username){
+        logger.info("Fetching users by username: " + username);
         Collection<User> payload = userRepository.findUserByUsernameContaining(username);
+
+        if(payload.isEmpty()){
+            logger.error("No users found");
+            return ResponseEntity.noContent().build();
+        }
+
         return ResponseEntity.ok().body(payload);
     }
 }
