c9ef2a745865fb3e7a0f9fbf529603bba281aafc
Nick <nicolas.cinera@gmail.com>
2022-10-21 22:52:20 -0400

    Docs Basic Setup with Swagger

--- src/main/java/com/body/improvement/club/BodyImprovementApplication.java
+++ src/main/java/com/body/improvement/club/BodyImprovementApplication.java
@@ -2,8 +2,10 @@ package com.body.improvement.club;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class BodyImprovementApplication {
 
 	public static void main(String[] args) {
