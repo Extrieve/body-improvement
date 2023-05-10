e6cda72844237597b2addbf71fade11072400f96
Nick <nicolas.cinera@gmail.com>
2023-05-09 23:43:47 -0400

    GeneralUtility

--- src/main/java/com/body/improvement/club/utility/GeneralUtility.java
+++ src/main/java/com/body/improvement/club/utility/GeneralUtility.java
@@ -2,17 +2,27 @@ package com.body.improvement.club.utility;
 
 import org.springframework.beans.BeanWrapper;
 import org.springframework.beans.BeanWrapperImpl;
+import org.springframework.stereotype.Component;
 
-import java.beans.FeatureDescriptor;
-import java.util.stream.Stream;
+import java.util.HashSet;
+import java.util.Set;
 
+@Component
 public class GeneralUtility {
 
-    public static String[] getNullPropertyNames(Object source) {
-        final BeanWrapper wrappedSource = new BeanWrapperImpl(source);
-        return Stream.of(wrappedSource.getPropertyDescriptors())
-                .map(FeatureDescriptor::getName)
-                .filter(propertyName -> wrappedSource.getPropertyValue(propertyName) == null)
-                .toArray(String[]::new);
+    public String[] getNullPropertyNames(Object source) {
+        final BeanWrapper src = new BeanWrapperImpl(source);
+        java.beans.PropertyDescriptor[] pds = src.getPropertyDescriptors();
+
+        Set<String> emptyNames = new HashSet<>();
+        for (java.beans.PropertyDescriptor pd : pds) {
+            Object srcValue = src.getPropertyValue(pd.getName());
+            // if null or not primitive or not string
+            if (srcValue == null || !pd.getPropertyType().isPrimitive()) {
+                emptyNames.add(pd.getName());
+            }
+        }
+        String[] result = new String[emptyNames.size()];
+        return emptyNames.toArray(result);
     }
 }
