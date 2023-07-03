a1feb32dc8a841798b2bb25dc74c635d7388c801
Nick <nicolas.cinera@gmail.com>
2023-07-03 18:57:29 -0400

    New Pageable route

--- src/main/java/com/body/improvement/club/controller/UserPageableController.java
+++ src/main/java/com/body/improvement/club/controller/UserPageableController.java
@@ -2,20 +2,18 @@ package com.body.improvement.club.controller;
 
 import com.body.improvement.club.entity.User;
 import com.body.improvement.club.service.UserPageableService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/v2/")
 @CrossOrigin(origins = "http://127.0.0.1:5173/", allowedHeaders = "*")
 public class UserPageableController {
 
     private final UserPageableService userPageableService;
-
-    public UserPageableController(UserPageableService userPageableService) {
-        this.userPageableService = userPageableService;
-    }
 
      @GetMapping(path = "/all", produces = "application/json")
     public ResponseEntity<Page<User>> findAllUsers(@RequestParam int page, @RequestParam int size){
@@ -30,5 +28,10 @@ public class UserPageableController {
     @GetMapping(path = "/find/{firstName}", produces = "application/json")
     public ResponseEntity<Page<User>> findUserByFirstName(@PathVariable String firstName, @RequestParam int page, @RequestParam int size){
         return userPageableService.getUsersByFirstNameContaining(firstName, page, size);
+    }
+
+    @GetMapping(path = "/find/{lastName}", produces = "application/json")
+    public ResponseEntity<Page<User>> findUserByLastName(@PathVariable String lastName, @RequestParam int page, @RequestParam int size){
+        return userPageableService.getUsersByLastNameContaining(lastName, page, size);
     }
 }
